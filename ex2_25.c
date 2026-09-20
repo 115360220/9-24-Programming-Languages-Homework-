2f629a410361bd35b31b11c8e7fb46001eabf33f
115360220 <t115360220@ntut.org.tw>
2026-09-20 16:13:32 +0800

    Rename to ex2_21 and add exercise 2.25

---
+++ ex2_25.c
@@ -0,0 +1,29 @@
+/* Exercise 2.25: Initials in Block Letters */
+#include <stdio.h>
+
+int main(void)
+{
+   printf("PPPPPPPPP\n");
+   printf("    P   P\n");
+   printf("    P   P\n");
+   printf("    P   P\n");
+   printf("     P P\n");
+
+   printf("\n");
+
+   printf("   JJ\n");
+   printf(" J\n");
+   printf("J\n");
+   printf(" J\n");
+   printf("   JJJJJJJ\n");
+
+   printf("\n");
+
+   printf("DDDDDDDDD\n");
+   printf("D       D\n");
+   printf("D       D\n");
+   printf(" D     D\n");
+   printf("  DDDDD\n");
+
+   return 0;
+}
