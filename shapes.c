de5ca7aa4bac5e125066897acaa8fcaad5867afb
115360220 <t115360220@ntut.org.tw>
2026-09-20 15:38:21 +0800

    Add exercise 2.21 shapes program

---
+++ shapes.c
@@ -0,0 +1,17 @@
+/* Exercise 2.21: Shapes with Asterisks */
+#include <stdio.h>
+
+int main(void)
+{
+   printf("*********       ***         *         *\n");
+   printf("*       *      *   *       ***       * *\n");
+   printf("*       *     *     *     *****     *   *\n");
+   printf("*       *    *       *      *      *     *\n");
+   printf("*       *   *         *     *     *       *\n");
+   printf("*       *    *       *      *      *     *\n");
+   printf("*       *     *     *       *       *   *\n");
+   printf("*       *      *   *        *        * *\n");
+   printf("*********       ***         *         *\n");
+
+   return 0;
+}
